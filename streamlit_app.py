79f22cddc31f61ebf0ac90d89492b8a4f218a24e
slowhare-digital <104913764+slowhare-digital@users.noreply.github.com>
2022-05-04 13:27:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick the fruits: ", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick the fruits: ", list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Didplay the table on the page
 streamlit.dataframe(my_fruit_list)
